668f2b3ef973fac109be115e4d7c3c1e0ed7e301
Nikhil Kutta <nikhilnitin3@gmail.com>
2024-10-05 15:06:59 +0530

    initial Setup

---
+++ server.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+ const  express = require('express');
+ const app=express();
+ const expressLayouts = require('express-ejs-layouts');
+
+ const mongoose=require('mongoose');
+ mongoose.connect('mongodb://localhost/DATABASE_URL');
+ console.log("database connected"); 
+
+ app.set('view engine', 'ejs');
+ app.set('views',__dirname+'/views');
+ app.set('layout','layoyts/layout');
+ app.use(expressLayouts);
+ app.use(express.static('public'));
+
+ app.use('/',require('./routes/index'));
+
+ app.listen(process.env.PORT||3000);
